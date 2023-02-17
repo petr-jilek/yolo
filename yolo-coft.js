0a7ca398ebd2f37d4a1c021e5ad4d69790c2af12
MrCoft <MrCoft@users.noreply.github.com>
2023-02-17 13:07:53 +0100

    Create yolo-coft.js

---
+++ yolo-coft.js
@@ -0,0 +1,20 @@
+
+// here's an idea for a feature for a yolo project in js on 5 lines
+/*
+    yolo-coft.js
+    
+    yolo-coft.js is a yolo proj
+    that yolo-coft.js is a yolo proj
+    that yolo-coft.js is a yolo proj
+    that yolo-coft.js is a yolo proj
+    that yolo-coft.js is a yolo proj
+*/
+
+// create fake tweet
+var tweet = {
+    text: "yolo-coft.js is a yolo proj that yolo-coft.js is a yolo proj that yolo-coft.js is a yolo proj that yolo-coft.js is a yolo proj that yolo-coft.js is a yolo proj",
+}
+
+// COPILOT BROKE
+// COPILOT SAY SOMETHING
+// 
